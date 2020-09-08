9dd07bfb1955a9df6a7b82d604f1da3963e38f87
ogo <oogochukwu315@gmail.com>
2020-09-08 12:31:28 +0100

    Created models and configured model admin site

--- cms/admin.py
+++ cms/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','author', 'description', 'pub_date')
+    list_filter = ('title', 'category',  'pub_date',)
+    search_fields = ['title', 'category__name',]
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
--- cms/models.py
+++ cms/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    """Model definition for Category."""
+
+    # TODO: Define fields here
+    name = models.CharField(max_length=250)
+
+    class Meta:
+        """Meta definition for Category."""
+
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    """Model definition for Post."""
+
+    # TODO: Define fields here
+    title = models.CharField(max_length=250)
+    author = models.CharField(max_length=250)
+    pub_date = models.DateField(auto_now_add=False)
+    category = models.ManyToManyField(Category, related_name='post')
+    description = models.TextField()
+
+    class Meta:
+        """Meta definition for Post."""
+
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+
+
+
+
+    
